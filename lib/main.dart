f9fe6661506f0a90d87e27ef3735c3bf5e361c80
Manoj Awasthi <awasthi.manoj@gmail.com>
2021-03-08 00:34:25 +0700

    Update themes and add shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Useless App',
       theme: ThemeData(
         // This is the theme of your application.
@@ -20,11 +22,13 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
+        canvasColor: Colors.lightGreen.shade100 ,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        platform: TargetPlatform.android,
       ),
       home: MyHomePage(title: 'Useless home page'),
     );
@@ -61,6 +65,13 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  void _updateSharedPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int counter = (prefs.getInt('counter') ?? 0) + 1;
+    print('Pressed $counter times');
+    await prefs.setInt('counter', counter);
   }
 
   @override
