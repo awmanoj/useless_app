a44f5f5988a6ccd1863d246da6b0f66905653659
Manoj Awasthi <awasthi.manoj@gmail.com>
2021-03-19 02:02:53 +0700

    Splash

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.lightGreen,
-        canvasColor: Colors.lightGreen.shade100 ,
+        primarySwatch: Colors.pink,
+        canvasColor: Colors.pink.shade300 ,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
